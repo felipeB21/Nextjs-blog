da9f4378fb6c5797be03c80933f32007097279b3
felipeB21 <bolgarfelipe@gmail.com>
2024-01-13 17:35:36 -0300

    Added fixed to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import GitHubIcon from "./GitHubIcon";
 
 export default function Header() {
   return (
-    <header className="py-3 border-b border-neutral-700">
+    <header className="fixed top-0 z-50 backdrop-blur-3xl bg-neutral-800 bg-opacity-70 w-full py-3 border-b border-neutral-700">
       <div className="w-[1000px] mx-auto flex items-center justify-between">
         <div>
           <Link className="text-xl font-medium" href="/">
